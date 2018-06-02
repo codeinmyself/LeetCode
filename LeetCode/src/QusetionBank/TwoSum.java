c89b096e6a965de10f2be567e2aa9bce4f231124
codeinmyself <1344863189@qq.com>
2018-06-02 17:11:46 +0800

    hh

---
+++ LeetCode/src/QusetionBank/TwoSum.java
@@ -0,0 +1,17 @@
+package QusetionBank;
+
+import java.util.ArrayList;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int[] twoSum(int[] nums, int target) {
+		ArrayList arr=new ArrayList();
+		return nums;
+    }
+
+}
